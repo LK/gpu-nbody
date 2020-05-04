584bbb2f5c2139494649befba7aa1f0b2b66b0ee
Vikram Mullick <vikram1.mullick@gmail.com>
2020-05-04 12:39:41 -0500

    moved tsne functions into separate file

--- src/simulator/cpu/nbodysim.c
+++ src/simulator/cpu/nbodysim.c
@@ -1,4 +1,5 @@
 #include "nbodysim.h"
+#include "tsneforces.c"
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -79,120 +80,6 @@ void dump(simdata_t *sdata, int step) {
   }
 }
 
-float getl2NormDiff(float *a, float *b, int dim)
-{
-    float norm = 0;
-    for (int i = 0; i < dim; i++)
-    {
-        float diff = a[i] - b[i];
-        norm += diff * diff;
-    }
-    return norm;
-}
-void setQjiDenominator(simdata_t *sdata, float *aux)
-{
-    float denominator = 0;
-    for (int k = 0; k < sdata->nparticles; k++)
-    {
-        for (int l = 0; l < sdata->nparticles; l++)
-        {
-            if (l == k)
-                continue;
-            denominator += 1 / (1 + getl2NormDiff(simdata_pos_ptr(sdata, l), simdata_pos_ptr(sdata, k), sdata->posdim));
-        }
-    }
-    aux[sdata->nparticles * sdata->nparticles] = denominator;
-}
-float getPji(int dataSize, int i, int j, float sd, float *l2NormDiffs)
-{
-    float denominator = 0;
-    for (int k = 0; k < dataSize; k++)
-    {
-        if (k == i)
-            continue;
-        denominator += exp(-l2NormDiffs[i * dataSize + k] / 2 / sd / sd);
-    }
-    float numerator = exp(-l2NormDiffs[i * dataSize + j] / 2 / sd / sd);
-    return numerator / denominator;
-}
-float getPerp(int dataSize, int i, float sd, float *l2NormDiffs)
-{
-    float shEntr = 0;
-    for (int j = 0; j < dataSize; j++)
-    {
-        float pji = getPji(dataSize, i, j, sd, l2NormDiffs);
-        shEntr -= (pji * log2(pji));
-    }
-    return pow(2.0, shEntr);
-}
-float findSd(int dataSize, int i, float *l2NormDiffs)
-{
-    float MIN_SD = 0.00000001;
-    float MAX_SD = 100000000;
-    float PERPLEXITY = 50;
-
-    float currentPerp = getPerp(dataSize, i, (MIN_SD + MAX_SD) / 2, l2NormDiffs);
-    while (fabsf(PERPLEXITY - currentPerp) > 0.01)
-    {
-        if (currentPerp > PERPLEXITY)
-        {
-            MAX_SD = (MIN_SD + MAX_SD) / 2;
-        }
-        else
-        {
-            MIN_SD = (MIN_SD + MAX_SD) / 2;
-        }
-        currentPerp = getPerp(dataSize, i, (MIN_SD + MAX_SD) / 2, l2NormDiffs);
-    }
-    float sd = (MAX_SD + MIN_SD) / 2;
-    return sd;
-}
-float* tsne_precompute(simdata_t *sdata, int dataSize)
-{
-  float *l2NormDiffs = malloc(sizeof(float) * dataSize * dataSize);
-  for (int i = 0; i < dataSize; i++)
-  {
-      for (int j = 0; j < dataSize; j++)
-      {
-          l2NormDiffs[i * dataSize + j] = getl2NormDiff(simdata_feat_ptr(sdata, i), simdata_feat_ptr(sdata, j), sdata->featdim);
-      }
-  }
-  
-  float *stdevs = malloc(sizeof(float) * dataSize);
-  for (int i = 0; i < dataSize; i++)
-  {
-      stdevs[i] = findSd(dataSize, i, l2NormDiffs);
-  }
-
-  float *aux = malloc(sizeof(float) * (dataSize * dataSize + 1));
-
-  for (int j = 0; j < dataSize; j++)
-  {
-      for (int i = 0; i < dataSize; i++)
-      {
-          aux[j * dataSize + i] = getPji(dataSize, i, j, stdevs[i], l2NormDiffs);
-      }
-  }
-
-  free(l2NormDiffs);
-  free(stdevs);
-  return aux;
-}
-void getTsneForce(float *force, float *position, int dataIndex,
-              float *positionActor, int actorDataIndex, simdata_t *sdata, float * aux) {
-
-  int dataSize = sdata->nparticles;
-  float pji = aux[actorDataIndex * dataSize + dataIndex];
-  float pij = aux[dataIndex * dataSize + actorDataIndex];
-  float qjiNumerator = 1 / (1 + getl2NormDiff(position, positionActor, sdata->posdim));
-  float qjiDenominator = aux[sdata->nparticles * sdata->nparticles];
-
-  float scalar = 4 * ((pij + pji) / 2 / dataSize - qjiNumerator / qjiDenominator) / (1 + getl2NormDiff(position, positionActor, sdata->posdim));
-
-  for (int i = 0; i < sdata->posdim; i++) {  
-      force[i] += scalar * (positionActor[i] - position[i]);
-  }
-}
 void run_simulation(simdata_t *sdata, simconfig_t *sconfig,
                     integrator_t int_type, force_t force_type, float time_step,
                     int steps) {
