798971639df4a6e528a06cd12ec1fc700671f80b
evanblasy <evanblasy@gmail.com>
2020-05-06 00:04:17 -0400

    added plots for GPU

--- scripts/galaxyplot.py
+++ scripts/galaxyplot.py
@@ -4,36 +4,71 @@ import numpy as np
 import subprocess
 
 cpu_data = []
-steps = 100
+gpu_data = []
+steps = 10
 subprocess.run(["make","test-galaxy-cpu"])
-# for i in [4096,8192,16384,32768,40960,49152]:
 for i in [128,256,512,1024,2048,4096,8192]:
 	outputs = subprocess.run(["./bin/test-galaxy-cpu " + str(steps) + " " + str(i)], shell=True,capture_output=True,encoding='utf-8')
 	tokens = outputs.stdout.split(", ")
 	tokens[0] = float(tokens[0])
 	tokens[1] = float(tokens[1])
 	tokens[2] = float(tokens[2])
+	tokens[3] = float(tokens[3])
 	tokens.append(i)
 	cpu_data.append(tokens)
 	print(i)
 
+for i in [128,256,512,1024,2048,4096,8192]:
+	outputs = subprocess.run(["./bin/test-galaxy-gpu " + str(steps) + " " + str(i)], shell=True,capture_output=True,encoding='utf-8')
+	tokens = outputs.stdout.split(", ")
+	tokens[0] = float(tokens[0])
+	tokens[1] = float(tokens[1])
+	tokens[2] = float(tokens[2])
+	tokens[3] = float(tokens[3])
+	tokens.append(i)
+	gpu_data.append(tokens)
+	print(i)
 
 
-x = np.array(cpu_data)[:,3]
-integrated_y = np.array(cpu_data)[:,1]
-force_calc = np.array(cpu_data)[:,0]
-total_time = np.array(cpu_data)[:,2]
 
-fig,a =  plt.subplots(2,2)
+x = np.array(cpu_data)[:,4]
+cpu_precomp = np.array(cpu_data)[:,0]
+cpu_integrated_y = np.array(cpu_data)[:,2]
+cpu_force_calc = np.array(cpu_data)[:,1]
+cpu_total_time = np.array(cpu_data)[:,3]
 
-a[0][0].plot(x,integrated_y)
-a[0][0].set_title('Integration Time')
-a[0][1].plot(x,force_calc)
-a[0][1].set_title('Force Calc Time')
-a[1][0].plot(x,total_time)
-a[1][0].set_title('Total Time')
-fig.tight_layout()
-plt.savefig("scripts/cpu-galaxy-fig.jpg")
+x = np.array(gpu_data)[:,4]
+gpu_precomp = np.array(cpu_data)[:,0]
+gpu_integrated_y = np.array(cpu_data)[:,2]
+gpu_force_calc = np.array(cpu_data)[:,1]
+gpu_total_time = np.array(cpu_data)[:,3]
+
+plt.plot(x,cpu_integrated_y,label='CPU')
+plt.plot(x,gpu_integrated_y,label='GPU')
+plt.title('Integration Time')
+plt.ylabel("Time (sec)")
+plt.xlabel("Num Bodies")
+plt.legend()
+plt.tight_layout()
+plt.savefig('plots/galaxy-fig-inte.jpg')
+plt.close()
+
+plt.plot(x,cpu_force_calc,label='CPU')
+plt.plot(x,gpu_force_calc,label='GPU')
+plt.title('Force Calc Time')
+plt.ylabel("Time (sec)")
+plt.xlabel("Num Bodies")
+plt.tight_layout()
+plt.savefig('plots/galaxy-fig-force.jpg')
+plt.close()
+
+plt.plot(x,cpu_total_time,label='CPU')
+plt.plot(x,gpu_total_time,label='GPU')
+plt.title('Total Time')
+plt.ylabel("Time (sec)")
+plt.xlabel("Num Bodies")
+plt.tight_layout()
+plt.savefig("plots/galaxy-fig.jpg")
 
 
 
